bc98b38297a702a53d56ccc26b2db617b3b3f408
Wang Han <1701210638@pku.edu.cn>
2024-06-29 23:49:21 +0800

    Update SuRequestViewModel.kt

--- app/src/main/java/com/topjohnwu/magisk/ui/surequest/SuRequestViewModel.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/surequest/SuRequestViewModel.kt
@@ -8,6 +8,7 @@ import android.content.res.Resources
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
